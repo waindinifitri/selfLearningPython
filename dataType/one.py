7ed048e35b4d423824d55fc7906bf5386e20e90c
Your Name <you@example.com>
2020-10-25 02:01:15 +0700

    variable and data types

---
+++ dataType/one.py
@@ -0,0 +1,18 @@
+character_name = "Dini"
+character_age = "24"
+is_male = False
+print("There once was a name named" + character_name + ",")
+print("she was " + character_age + " years old. ")
+
+character_name = "Kafka"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ",")
+
+print("Glints\nAcademy")
+word = "Glints Academy"
+print(word + " is tremendously cool")
+print(word.lower())
+print(word.upper())
+print(word.upper().isupper())
+print(len(word))
+print(word[0])
