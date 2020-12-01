1716da7088d2f5d49cc44793b881e6996d32c893
Waindini Nur Fitri <waindini@gmail.com>
2020-12-02 04:53:42 +0700

    assignment_2

--- PTStask/PTS_SE_WaindiniNurFitri.ts
+++ PTStask/PTS_SE_WaindiniNurFitri.ts
@@ -17,34 +17,154 @@
  * 15. Most spender buyer = Math.max[trasaction] => return to buyer who spend most spender;
  */
 // import {Input} from './Input.ts'
-class items {
-    name: string = "";
-    type: string = "hats" || "tops" || "shorts";
-    prices: number; //depends on buyer
+const input =
+{
+    Items:
+        [
+            {
+                name: 'oval hat', // product name
+                type: 'hats', // product type
+                prices: [
+                    {
+                        priceFor: 'regular', // price is valid for
+                        price: 20000, // the price
+                    },
+                    {
+                        priceFor: 'VIP', // price is valid for
+                        price: 15000, // the price
+                    },
+                ]
+            }, {
+                name: 'square hat', // product name
+                type: 'hats', // product type
+                prices: [
+                    {
+                        priceFor: 'regular', // price is valid for
+                        price: 30000, // the price
+                    },
+                    {
+                        priceFor: 'VIP', // price is valid for
+                        price: 20000, // the price
+                    },
+                    {
+                        priceFor: 'wholesale', // price is valid for
+                        price: 15000, // the price					
+                    }
+                ]
+            }, {
+                name: 'magic shirt', // product name
+                type: 'tops', // product type
+                prices: [
+                    {
+                        priceFor: 'regular', // price is valid for
+                        price: 50000, // the price
+                    }
+                ]
+            }
+        ],
+    Buyers:
+        [
+            {
+                name: 'Ani', // buyer name
+                type: 'regular', // buyer type - VIP, regular, wholesale
+            }, {
+                name: 'Budi', // buyer name
+                type: 'VIP', // buyer type - VIP, regular, wholesale
+            }, {
+                name: 'Charlie',
+                type: 'regular'
+            }, {
+                name: 'Dipta',
+                type: 'wholesale'
+            }
+        ],
+    Transaction:
+        [
+            {
+                item: 'magic shirt', // product name
+                qty: 1, // buying quantity
+                buyer: 'Ani' // buyer name
+            }, {
+                item: 'square hat', // product name
+                qty: 2, // buying quantity
+                buyer: 'Budi' // buyer name
+            }, {
+                item: 'magic shirt', // product name
+                qty: 1, // buying quantity
+                buyer: 'Ani' // buyer name
+            }, {
+                item: 'oval hat', // product name
+                qty: 1, // buying quantity
+                buyer: 'Ani' // buyer name
+            }, {
+                item: 'square hat',
+                qty: 100,
+                buyer: 'Dipta'
+            },
+        ]
 }
 
-class buyers {
-    name: string = "";
-    type: string = "VIP" || "regular" || "wholesale";
-    prices: number; //depends on buyer
+const totalNumberTransaction = (data) => {
+    return data.Transaction.length;
+};
+console.log('totalTransactions: ', totalNumberTransaction(input), 'transactions');
 
-    price() {
-        if (this.type === "regular") {
-            console.log(`The customer have an actualPrice!`)
-        } else if (this.type === "VIP") {
-            console.log(`The customer is a ${this.type}, they have a special offer! `)
-        } else if (this.type === "wholesale") {
-            console.log("Something went wrog!")
+var bestSellingItemName = '';
+const bestSellingItems = (array) => {
+    let newArray = [];
+    array.Transaction.forEach(item => {
+        if (!isExist(newArray, item.item, null).found) {
+            newArray.push({
+                item: item.item,
+                qty: item.qty
+            });
+        } else {
+            const index = isExist(newArray, item.item, null).index;
+            newArray[index].qty = newArray[index].qty + item.qty;
         }
+    });
+    const sortedArray = newArray.sort((a, b) => (a.qty < b.qty) ? 1 : -1);
+    const bestSellingItem = sortedArray[0].item;
+    bestSellingItemName = sortedArray[0].item;
+    return bestSellingItem;
+};
+console.log('bestSellingItem: ', bestSellingItems(input));
+
+
+const bestSellingCategory = (array, bestSellingItemsName) => {
+    if (isExist(array, null, bestSellingItemsName).found) {
+        const index = isExist(array, null, bestSellingItemsName).index;
+        const data = array[index].type;
+        return data;
+    }
+};
+
+console.log('bestSellingCategory: ', bestSellingCategory(input.Items, bestSellingItemName));
+
+function isExist(arr, item, name) {
+    if (item) {
+        const found = arr.some(el => el.item === item);
+        const index = arr.findIndex(el => el.item === item);
+        return {
+            found: found,
+            index: index
+        };
+    } else if (name) {
+        const found = arr.some(el => el.name === name);
+        const index = arr.findIndex(el => el.name === name);
+        return {
+            found: found,
+            index: index
+        };
     }
 }
 
-class transaction {
-    item: string = "hats" || "tops" || "shorts";
-    qty: number;
-    buyer: string = "VIP" || "regular" || "wholesale";
+const revenuePerCategory = (price, Items) => {
+    let rpc = [];
+
 }
 
-// const totalNumberTransaction = () => {
-
-// }
+const revenue = (data) => {
+    return data.Transaction.length;
+};
+console.log('revenue: ', revenue(input));
